069292d0738eec6701af9fc4f8a104343b03592e
Hesham Massoud <hesham.massoud@commercetools.de>
2015-08-25 17:41:34 +0200

    SUN-76: Add seeSizeGuide Gherkins Feature and FluentCucumberTest

---
+++ src/main/java/io/commercetools/sunrise/steps/SeeSizeGuide.java
@@ -0,0 +1,46 @@
+package io.commercetools.sunrise.steps;
+
+import io.commercetools.sunrise.pages.PDP;
+import org.fluentlenium.core.annotation.Page;
+import org.fluentlenium.cucumber.adapter.FluentCucumberTest;
+import org.fluentlenium.cucumber.adapter.util.SharedDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import cucumber.api.java8.En;
+
+@SharedDriver(type = SharedDriver.SharedType.PER_SCENARIO)
+public class SeeSizeGuide extends FluentCucumberTest implements En {
+
+	@Page
+	private PDP pdp;
+
+	@Override
+	public WebDriver getDefaultDriver() {
+		return new HtmlUnitDriver();
+	}
+
+	public SeeSizeGuide() {
+		Before(()-> {
+			this.initFluent();
+			this.initTest();
+		});
+
+		Given("I am on a random product's pdp", () -> {
+			pdp.goToRandomProduct(this);
+			pdp.isAt();
+        });
+
+		When("I click on the size guide button", () -> {
+			pdp.clickOnSizeGuideButton();
+		});
+
+		Then("I should see a modal window containing international size conversions", ()-> {
+			pdp.seeSizeGuideModal();
+		});
+
+		After(()-> {
+			this.quit();
+		});
+
+	}
+}
---
+++ src/main/resources/features/seeSizeGuide.feature
@@ -0,0 +1,10 @@
+Feature: Product Size Guide
+  The user should be able to see a size guide for all categories
+  of products on the web shop in a modal window when he/she clicks 
+  on the size guide button in the pdp (product detail page) of any product.
+    
+  @ignore  
+  Scenario: I can see the size guide for a product
+    Given I am on a random product's pdp
+    When I click on the size guide button
+    Then I should see a modal window containing international size conversions
